6ec24608da740058b76c418b34fbbd88b5133133
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2024-05-17 18:27:02 +0700

    upload login

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+import { Card, Image } from "react-bootstrap";
 import logo from "./logo.svg";
 import "./App.css";
 import { LoginPage } from "./components/user/login";
@@ -6,10 +9,45 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState("");
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <LoginPage />
-      <ToastContainer />
+    <div class="container">
+      <Card style={{ width: "600px" }}>
+        <Card.Header>
+          {!login && (
+            <FacebookLogin
+              appId="1253008412343322"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook"
+            />
+          )}
+          {login && <Image src={picture} roundedCircle />}
+        </Card.Header>
+        {login && (
+          <Card.Body>
+            {console.log(data)}
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>{data.email}</Card.Text>
+          </Card.Body>
+        )}
+      </Card>
     </div>
   );
 }
