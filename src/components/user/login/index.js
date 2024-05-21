86e3836a31a6624f683f5c517384434133eeab55
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2024-05-21 11:01:51 +0700

    upload id

--- src/components/user/login/index.js
+++ src/components/user/login/index.js
@@ -110,8 +110,8 @@ export const LoginPage = () => {
               </h4>
               <div>
                 <FacebookLogin
-                  // appId="1253008412343322"
-                  appId="473464405251591"
+                  appId="1253008412343322"
+                  // appId="473464405251591"
                   autoLoad={false}
                   fields="name,email,picture"
                   scope="email,user_photos,user_posts,user_birthday,user_hometown,user_location,user_likes,user_events,user_videos,user_friends,user_gender,user_link,user_age_range,manage_fundraisers,public_profile"
