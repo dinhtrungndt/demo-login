1934777cffafe402b4ecfb23504388eb5f84d93e
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2024-05-23 17:17:57 +0700

    upload infor friend

--- src/components/home/user/listFriend/index.js
+++ src/components/home/user/listFriend/index.js
@@ -18,7 +18,7 @@ export const ListFriend = () => {
     }
   };
 
-  console.log("friends", friends.summary.total_count);
+  //   console.log("friends", friends.summary.total_count);
 
   useEffect(() => {
     onGetFriends();
@@ -27,14 +27,21 @@ export const ListFriend = () => {
   return (
     <div>
       <h1 className="font-bold">
-        Tổng bạn bè của bạn là: {friends.summary.total_count}
+        Tổng bạn bè của bạn là: {friends?.summary?.total_count}
       </h1>
-      {friends.data.map((friend) => (
-        <div key={friend.id}>
-          <h3>{friend.name}</h3>
-          <img src={friend.picture.data.url} alt={friend.name} width={50} />
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4 p-2">
+        {friends?.data?.map((friend) => (
+          <div key={friend.id}>
+            <h3>{friend.name}</h3>
+            <img
+              src={friend.picture.data.url}
+              alt={friend.name}
+              width={50}
+              className="rounded-full"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
